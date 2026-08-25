3e47cccac7728c41e3dd4d2b0c2bb93f3262d384
lagaylayicmae <lagaylayicmae@gmail.com>
2026-08-25 10:20:48 +0800

    Hello.c
    Added comments to clarify code functionality.

--- Hello.c
+++ Hello.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // Allows the use of print() function for displaying output
 
-int main()
+int main() // The main function where the program starys
 {
-    printf("Name: Ic Mae Z. LagayLay\n");
+    printf("Name: Ic Mae Z. LagayLay\n"); // Displays student's name 
     printf("Name: Karyll Haze G. Galendez\n");
-    printf("Course: BSIT\n");
-    printf("Section: 1-A\n");
+    printf("Course: BSIT\n"); // Displays  the course information 
+    printf("Section: 1-A\n"); // Display the section information 
 
-    return 0;
+    return 0; // Ends the program successfully 
 }
