86d0b459ad332e1f55bfbbf777f72e683bd5c04c
lagaylayicmae <lagaylayicmae@gmail.com>
2026-08-25 09:29:28 +0800

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name: Ic Mae Z. LagayLay\n");
+    printf("Name: Karyll Haze G. Galendez\n");
+    printf("Course: BSIT\n");
+    printf("Section: 1-A\n");
+
+    return 0;
+}
