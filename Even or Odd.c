f2163a8c30200bca99b1bc824209e0ab82413377
lagaylayicmae <lagaylayicmae@gmail.com>
2026-08-25 10:30:03 +0800

    Even or Odd
    Added comments to explain the code logic and functionality.

--- Even or Odd.c
+++ Even or Odd.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include <stdio.h> // Allows the program to use printf() for displaying output. 
 
-int main(){
-    int number =  7;
+int main() { // The main function where the program starts executing. 
+    int number =  7; // Create an integers variables named number and stores the value 7.
 
-    if (number % 2 == 0 ) {
-         printf("%d is Even\n", number);
+    if (number % 2 == 0 ) { // Checks if the numberd is divisible by 2 with no remainder.
+         printf("%d is Even\n", number); // Displays the number as Even if the condition is true. 
     } else {
-         printf("%d is Odd\n", number);
+         printf("%d is Odd\n", number); // Displays the number as Odd if the conditions is false. 
     }
 
-    return 0;
-}
+    return 0; // Ends the program and indicates that it ran successfully. 
