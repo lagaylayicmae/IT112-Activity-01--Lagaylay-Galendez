7868a9bb4c6a17eb1072bac6fae135cc0276039f
lagaylayicmae <lagaylayicmae@gmail.com>
2026-08-25 10:38:41 +0800

    Addition
    Added comments to explain the code functionality.

--- Addition.c
+++ Addition.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h> // Allows the program to use input/output functions, especially printf(). 
 
-int main() {
+int main() { // This is the main function. It is where the program starts running. 
 
-    int num1 = 12;
-    int num2 = 8;
-    int sum;
+    int num1 = 12; // Stores the first number, 12.
+    int num2 = 8; // Stores the second number, 8.
+    int sum; // It will stores the result of adding the two numbers. 
 
-    sum = num1 + num2;
+    sum = num1 + num2; // Adds num1 and num2 together and stores the result in sum. 
 
-    printf("First number: %d\n", num1);
-    printf("Second number : %d\n", num2);
-    printf("Sum: %d\n", sum);
+    printf("First number: %d\n", num1); // Displays the value of num1. 
+    printf("Second number : %d\n", num2); // Displays the value of num2. 
+    printf("Sum: %d\n", sum); // Displays the caluculated sum. 
 
-    return 0;
+    return 0; // Ends the program and indicates that it ran successfully. 
 }
